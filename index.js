07e97d7e1540316f8bbff2fd7288b47c7fe4d3c2
doris hermawan <dorizo89@gmail.com>
2022-05-15 23:05:24 +0700

    d

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var http = require('http');
-
+const hostname = '3.84.2.133';
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-}).listen(8080);
+}).listen(3000 , hostname , () => {
+
+    console.log(`Server running at http://${hostname}:${port}/`);
+  
+  });
